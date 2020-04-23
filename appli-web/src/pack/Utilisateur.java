e915972128fa64b8c4c9ccaa2a5b9c2a8790524c
Claire Maurice <claire.maurice98@gmail.com>
2020-04-23 16:48:58 +0200

    Classe java Utilisateur

---
+++ appli-web/src/pack/Utilisateur.java
@@ -0,0 +1,49 @@
+package pack;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Utilisateur {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)  
+	String pseudo;
+	String email;
+	String mdp;
+	
+	
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	
+
+}
